55f0886b6b8382e103e0a41dc0ea8be4e92fcd0b
mistermooster11 <mistermooster11@gmail.com>
2026-05-02 08:18:10 +0700

    Update mainNavItems.ts

--- lib/constants/mainNavItems.ts
+++ lib/constants/mainNavItems.ts
@@ -1,21 +1,17 @@
-export type NavDropdownLink = {
-  label: string;
-  href: string;
-  external?: boolean;
-};
-
 export type NavDropdown = {
   title: string;
   titleHref: string;
-  links: NavDropdownLink[];
-  quickLinks?: NavDropdownLink[];
+  description: string;
+  items?: Array<{ label: string; href: string; external?: boolean }>;
+  links: Array<{ label: string; href: string; external?: boolean }>;
+  quickLinks?: Array<{ label: string; href: string; external?: boolean; icon: string }>;
 };
 
 export type MainNavItem = {
   label: string;
   href?: string | null;
   external?: boolean;
-  dropdown?: NavDropdown;
+  dropdown?: NavDropdown | null;
 };
 
 export const mainNavItems: MainNavItem[] = [
